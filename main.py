a4efa556b17e353ad7e883bd505cfc5908d684e3
Gor Avetisyan <146785439+GoranAvetisyan@users.noreply.github.com>
2024-12-27 21:39:33 +0400

    Update main.py
    Add CRUD - STUDENT TABLE

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+from fastapi import FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
+from sessions import session as db_session
+import models as db_models
+from sqlalchemy import DateTime
+from datetime import date
+from fastapi import Query
+from sqlalchemy import func
+
+app = FastAPI()
+
+# CRUD - STUDENT TABLE
+
+@app.post("/add_student", tags=["student"])
+async def add_student(full_name: str, grade_point_average: float, student_age: int,
+                      year_of_entry: int, student_gender: str):
+    student_obj = db_models.Student(Name_Surname=full_name, gpa=grade_point_average, age=student_age,
+                                    entry_year=year_of_entry, gender=student_gender)
+    db_session.add(student_obj)
+    db_session.commit()
+    return f"Student Added: {student_obj.id} - {student_obj.Name_Surname}."
+
+
