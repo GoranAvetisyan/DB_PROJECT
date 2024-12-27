825c9e395d01a79102a4e2439002319b361f1832
Gor Avetisyan <146785439+GoranAvetisyan@users.noreply.github.com>
2024-12-27 22:29:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,37 @@ async def get_student(student_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Failed to get student by ID: Student not found.")
     return student
+
+
+
+@app.get("/get_all_students", tags=["student"])
+async def get_all_students(skip: int = 0, limit: int = 100):
+    students_query = session_.query(models_.Student).offset(skip).limit(limit)
+    return students_query.all()
+
+@app.put("/update_student/{student_id}", tags=["student"])
+async def update_studnet(student_id_: int, new_name_srn_: str, new_gpa_: float, new_age_: int, new_entry_year: int, gender_: str = ""):
+    if (obj := session_.query(models_.Student).filter(models_.Student.id == student_id_).first()) is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Failed to update student: No student with such ID.")
+    if new_name_srn_:
+        obj.Name_Surname = new_name_srn_
+    if new_gpa_:
+        obj.gpa = new_gpa_
+    if new_age_:
+        obj.age = new_age_
+    if new_entry_year:
+        obj.entry_year = new_entry_year
+    if gender_:
+        obj.gender = gender_
+    session_.add(obj)
+    session_.commit()
+    return f"Successfully updated student with ID:{obj.id}."
+
+@app.delete("/delete_student/{student_id}", tags=["student"])
+async def delete_student(student_id_: int):
+    if (obj := session_.query(models_.Student).filter(models_.Student.id == student_id_).first()) is not None:
+        session_.delete(obj)
+        session_.commit()
+        
+        return f"Student deleted: {obj.id} - {obj.Name_Surname}."
