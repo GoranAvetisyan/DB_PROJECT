d99be16b2f1e3ff2d9980b62907e622a238ab18a
Gor Avetisyan <146785439+GoranAvetisyan@users.noreply.github.com>
2024-12-28 20:39:31 +0400

    Update session.py
    add create_flag,url

--- session.py
+++ session.py
@@ -13,3 +13,32 @@ db_host = "localhost"
 db_port = "5432"
 db_name = "db_proj"
 owner_user = "postgres"
+
+
+
+if create_flag:
+    default_connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/"
+    default_engine = create_engine(default_connection_string, isolation_level="AUTOCOMMIT")
+
+    with default_engine.connect() as connection:
+        db_creation_query = text(f"CREATE DATABASE {db_name} WITH OWNER {owner_user}")
+
+        try:
+            connection.execute(db_creation_query)
+            print(f"Database '{db_name}' created successfully.")
+
+        except exc.ProgrammingError as err:
+            print(f"Error creating database '{db_name}': {err}")
+
+url = URL.create(
+    drivername="postgresql",
+    username=db_user,
+    password=db_password,
+    host=db_host,
+    database=db_name,
+    port=int(db_port)
+)
+
+engine = create_engine(url)
+Session = sessionmaker(bind=engine)
+session = Session()
